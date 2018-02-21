f98521573c60e9c032ab06e61603c8477f693d0c
Jak <dema6685@kettering.edu>
2018-02-20 19:10:22 -0500

    Character and Game creation
    new class creation

---
+++ src/Character.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Dominic DeMaria(Dema6685) on 2/19/2018.
+ * CS-203 10:15 -11:50
+ **/
+public class Character {
+    String name;//Name of character
+    public Character(String name)
+    {
+        this.name = name;
+    }//end of Character
+}//End of Class Character
---
+++ src/Game.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Dominic DeMaria(Dema6685) on 2/19/2018.
+ * CS-203 10:15 -11:50
+ **/
+public class Game {
+
+}
